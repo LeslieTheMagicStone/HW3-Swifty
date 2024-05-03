5c75d710ae187569b79afa0466c32f525d9236d9
LeslieTheMagicStone <lesliethemagicstone@gmail.com>
2024-05-03 23:13:40 +0800

    feat: update logo animation v1.1

--- Assets/Scripts/LogoAnim.cs
+++ Assets/Scripts/LogoAnim.cs
@@ -1,30 +1,26 @@
-using System.Collections;
 using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
 public class LogoAnim : MonoBehaviour
 {
-    TMP_Text logoName;
-    const float DURATION = 1.0f;
+    [SerializeField] TMP_Text logoName;
+    [SerializeField] TMP_Text logo;
 
-    IEnumerator Start()
+    private void Start()
     {
-        logoName = GetComponent<TMP_Text>();
-        float timer = 0.0f;
+        logo.color = new(logo.color.r, logo.color.g, logo.color.b, 0.0f);
+        logoName.color = new(logoName.color.r, logoName.color.g, logoName.color.b, 0.0f);
+        float targetPosX = logo.transform.position.x;
+        logo.transform.localPosition = Vector3.zero;
 
-        while (timer < DURATION)
-        {
-            timer += Time.deltaTime;
-            float scaler = timer / DURATION;
-            Color color = logoName.color;
-            color.a = scaler;
-            logoName.color = color;
-            yield return null;
-        }
-
-        yield return new WaitForSeconds(1);
-
-        SceneController.Instance.LoadNextScene();
+        Sequence sequence = DOTween.Sequence();
+        sequence.Append(logo.DOFade(1, 0.5f));
+        sequence.Append(logo.transform.DOMoveX(targetPosX, 0.5f));
+        sequence.Append(logoName.DOFade(1, 0.5f));
+        sequence.AppendInterval(0.3f);
+        sequence.Append(logo.DOFade(0, 0.3f));
+        sequence.Join(logoName.DOFade(0, 0.3f));
+        sequence.AppendCallback(() => SceneController.Instance.LoadNextScene());
     }
-
 }

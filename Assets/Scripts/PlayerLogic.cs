1f853790d0e4b34944ced5e943d5eb2905f3754e
LeslieTheMagicStone <lesliethemagicstone@gmail.com>
2024-05-18 01:14:33 +0800

    feat: add grid-based player movement

--- Assets/Scripts/PlayerLogic.cs
+++ Assets/Scripts/PlayerLogic.cs
@@ -2,24 +2,32 @@ using UnityEngine;
 
 public class PlayerLogic : MonoBehaviour
 {
+    [SerializeField] Transform detectionRayOrigin;
     Animator animator;
     CharacterController characterController;
     float horizontalInput, verticalInput;
     float horizontalInputRaw, verticalInputRaw;
-    Vector3 movement;
     Vector3 rotation;
+    Vector3 targetPos;
+    float moveTimer;
+    bool isMoving => moveTimer > 0f;
 
-    const float SPEED = 2f;
-    const float ANGULAR_SPEED = 120f;
+    const float MOVE_TIME = 0.2f;
+    const float MOVEMENT_DISTANCE = 1f;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
         characterController = GetComponent<CharacterController>();
+
+        targetPos = transform.position;
+        moveTimer = 0f;
     }
 
     private void Update()
     {
+        UpdateTimers();
+
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
@@ -33,15 +41,28 @@ public class PlayerLogic : MonoBehaviour
         animator.SetBool("OnRight", Input.GetKey(KeyCode.RightArrow));
         animator.SetBool("OnDown", Input.GetKey(KeyCode.DownArrow));
         animator.SetBool("OnUp", Input.GetKey(KeyCode.UpArrow));
+
+        Vector3 direction = Vector3.zero;
+        if (Input.GetButtonDown("Movement Left")) direction = Vector3.left;
+        if (Input.GetButtonDown("Movement Right")) direction = Vector3.right;
+        if (Input.GetButtonDown("Movement Up")) direction = Vector3.forward;
+        if (Input.GetButtonDown("Movement Down")) direction = Vector3.back;
+        if (direction != Vector3.zero && !isMoving)
+        {
+            moveTimer = MOVE_TIME;
+            Ray ray = new(detectionRayOrigin.position, direction);
+            if (!Physics.Raycast(ray, out RaycastHit hit, MOVEMENT_DISTANCE))
+                targetPos += direction * MOVEMENT_DISTANCE;
+        }
+
+        Vector3 nextPos = Vector3.Lerp(transform.position, targetPos, Time.deltaTime * 10f);
+        characterController.Move(nextPos - transform.position);
     }
 
-    private void FixedUpdate()
+
+    private void UpdateTimers()
     {
-        var groundMovement = new Vector3(horizontalInput, 0, verticalInput).normalized * SPEED * Time.deltaTime;
-        groundMovement.x *= Mathf.Abs(horizontalInput);
-        groundMovement.z *= Mathf.Abs(verticalInput);
-        movement.x = groundMovement.x;
-        movement.z = groundMovement.z;
-        characterController.Move(movement);
+        if (moveTimer > 0) moveTimer -= Time.deltaTime;
+        else moveTimer = 0f;
     }
 }

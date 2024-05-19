48dd1d5361d0f3f6f53b496e673bb900b89a968d
LeslieTheMagicStone <lesliethemagicstone@gmail.com>
2024-05-19 19:33:00 +0800

    feat: change camera

--- Assets/Scripts/CameraLogic.cs
+++ Assets/Scripts/CameraLogic.cs
@@ -4,18 +4,17 @@ public class CameraLogic : MonoBehaviour
 {
     Transform player;
 
-    const float Y_OFFSET = 8f;
-    const float Z_OFFSET = -5f;
+    Vector3 offset;
     const float SMOOTH_FACTOR = 2f;
 
     private void Start()
     {
         player = FindObjectOfType<PlayerLogic>().transform;
+        offset = transform.position - player.position;
     }
 
     private void Update()
     {
-        Vector3 offset = new(0, Y_OFFSET, Z_OFFSET);
         Vector3 targetPos = player.position + offset;
         transform.position = Vector3.Lerp(transform.position, targetPos, SMOOTH_FACTOR * Time.deltaTime);
     }

f7275087de7e16256fd076fba58a43bfc5bee6a3
LeslieTheMagicStone <lesliethemagicstone@gmail.com>
2024-05-20 14:36:23 +0800

    feat: update dielogic

--- Assets/Scripts/DieLogic.cs
+++ Assets/Scripts/DieLogic.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using UnityEngine;
+using DG.Tweening;
 
 public class DieLogic : MonoBehaviour
 {
+    AudioLowPassFilter filter;
     private IEnumerator Start()
     {
         Time.timeScale = 0.5f;
+        DOTween.timeScale = 0.5f;
+
+        filter = FindObjectOfType<AudioLowPassFilter>();
+        DOVirtual.Float(1f, 0f, 1f, (t) => filter.cutoffFrequency = 5000f * t * t);
+
         yield return new WaitForSecondsRealtime(2f);
+
         Time.timeScale = 1f;
+        DOTween.timeScale = 1f;
         yield return VignetteLogic.Instance.Outro();
         SceneController.Instance.ReloadScene();
     }
